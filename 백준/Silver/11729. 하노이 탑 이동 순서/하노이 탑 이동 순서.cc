4d9fb7777deed2d2a9163cc3403bdc4854fd9caf
wseung123 <118625430+wseung123@users.noreply.github.com>
2023-08-20 14:47:58 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector> 
+#include<stack>
+#include<string>
+#include<list>
+#include<queue>
+#include<deque>
+#include<tuple>
+using namespace std;
+
+void func(int a, int b ,int n)
+{
+    if (n == 1) { cout << a << ' ' << b << '\n'; return; }
+    func(a, 6 - a - b, n - 1);
+    cout << a << ' ' << b << '\n';
+    func(6 - a - b, b, n - 1);
+}
+
+
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int k;
+    cin >> k;
+    cout << (1 << k) - 1 << '\n';
+    func(1, 3, k);
+    
+}
